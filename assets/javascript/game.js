394384f7a4e105103dc88792fb3fa3db89d7c4eb
hmalle <hmalle90ak@gmail.com>
2018-01-20 21:30:27 -0800

    got the click on working and rearranging the pictures

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-
+/* I think there might be a better way to manage these data*/
 var character1=
 {
   name: "Obi-Wan Kenobi",
@@ -27,8 +27,42 @@ var character4=
   attackPower: 10,
   counterAttackPower: 10
 };
+/*
+var player1 = $(".character1");
+var player2 = $(".character2");
+var player3 = $(".character3");
+var player4 = $(".character4");
+*/
+/*
+function updateCharacterInfo()
+{
+  $(".character1").$("healthPoints").html(character1.healthPoints);
+}
+*/
+
+var characterSelected = false;
+var enemySelected = false;
 
 $(document).ready(function()
 {
-  
+  $(".character").on("click", function() //listens for clicks on the character only
+  {
+    if( !characterSelected )
+    {
+      var player = $(this);    
+      $(".allCharacters").hide();
+      var enemies = player.siblings();
+      $(".yourCharacter").html(player);
+      $(".enemies").html(enemies);
+      $(".enemies").css("background-colo: red");
+      characterSelected = true;
+    }
+    else if(!enemySelected)
+    {
+      $(".enemiesSection").hide($(this));
+      $(".defenderSection").html(this);
+      
+      enemySelected = true;
+    }
+  });
 });
