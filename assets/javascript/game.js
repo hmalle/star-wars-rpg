b637098a272f8596d1674e12a693c8e500404e5b
hmalle <hmalle90ak@gmail.com>
2018-01-20 17:19:49 -0800

    started a bit of coding and added the images

---
+++ assets/javascript/game.js
@@ -0,0 +1,34 @@
+
+var character1=
+{
+  name: "Obi-Wan Kenobi",
+  healthPoints: 120,
+  attackPower: 10,
+  counterAttackPower: 10
+};
+var character2=
+{
+  name: "Luke Skywalker",
+  healthPoints: 100,
+  attackPower: 10,
+  counterAttackPower: 10
+};
+var character3=
+{
+  name: "Darth Sidious",
+  healthPoints: 150,
+  attackPower: 10,
+  counterAttackPower: 10
+};
+var character4= 
+{
+  name: "Darth Maul",
+  healthPoints: 180,
+  attackPower: 10,
+  counterAttackPower: 10
+};
+
+$(document).ready(function()
+{
+  
+});
